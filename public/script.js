8cc149241503fd4ba70f32154915c3107dea113f
edhasingh125 <edha.singh.s.125@kalvium.community>
2026-07-19 19:51:30 +0530

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,192 @@
+const API = "http://localhost:3000";
+
+async function register(){
+
+    const username=document.getElementById("username").value;
+    const password=document.getElementById("password").value;
+
+    const res=await fetch(API+"/register",{
+
+        method:"POST",
+
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify({
+            username,
+            password
+        })
+
+    });
+
+    const data=await res.json();
+
+    alert(data.message);
+
+    if(data.message=="Registration Successful"){
+        window.location="login.html";
+    }
+
+}
+
+
+async function login(){
+
+    const username=document.getElementById("username").value;
+
+    const password=document.getElementById("password").value;
+
+    const res=await fetch(API+"/login",{
+
+        method:"POST",
+
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify({
+            username,
+            password
+        })
+
+    });
+
+    const data=await res.json();
+
+
+    if(data.message=="Login Successful"){
+
+        localStorage.setItem("userId",data.userId);
+
+        window.location="dashboard.html";
+
+    }
+    else{
+
+        alert(data.message);
+
+    }
+
+}
+
+async function addTask(){
+
+    const title=document.getElementById("task").value;
+
+    const userId=localStorage.getItem("userId");
+
+    const res=await fetch(API+"/tasks",{
+
+        method:"POST",
+
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify({
+            title,
+            userId
+        })
+
+    });
+
+    document.getElementById("task").value="";
+
+    loadTasks();
+
+}
+
+async function loadTasks() {
+
+    const userId = localStorage.getItem("userId");
+
+    const res = await fetch(API + "/tasks/" + userId);
+
+    const tasks = await res.json();
+
+    let output = "";
+
+    tasks.forEach(task => {
+
+        output += `
+        <div style="border:1px solid #ccc; padding:10px; margin:10px 0; border-radius:5px;">
+
+            <h3>${task.completed ? "✅" : "📌"} ${task.title}</h3>
+
+            <button onclick="editTask('${task._id}','${task.title}')">Edit</button>
+
+            <button onclick="completeTask('${task._id}')">
+                ${task.completed ? "Completed" : "Complete"}
+            </button>
+
+            <button onclick="deleteTask('${task._id}')">
+                Delete
+            </button>
+
+        </div>
+        `;
+
+    });
+
+    document.getElementById("taskList").innerHTML = output;
+
+}
+
+async function deleteTask(id){
+
+    await fetch(API+"/tasks/"+id,{
+        method:"DELETE"
+    });
+
+    loadTasks();
+
+}
+
+async function editTask(id, oldTitle){
+
+    const newTitle = prompt("Edit Task", oldTitle);
+
+    if(!newTitle) return;
+
+    await fetch(API + "/tasks/" + id,{
+
+        method:"PUT",
+
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify({
+            title:newTitle
+        })
+
+    });
+
+    loadTasks();
+
+}
+
+async function completeTask(id){
+
+    await fetch(API + "/tasks/" + id,{
+
+        method:"PUT",
+
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:JSON.stringify({
+            completed:true
+        })
+
+    });
+
+    loadTasks();
+
+}
+
+if(window.location.pathname.includes("dashboard.html")){
+    loadTasks();
+}
---
+++ server.js
@@ -0,0 +1,147 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const User = require("./models/User");
+const Task = require("./models/Task");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+    res.send("Todo API Running");
+});
+
+app.get("/test", (req, res) => {
+    res.send("Test route works");
+});
+
+app.post("/register", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        const userExists = await User.findOne({ username });
+
+        if (userExists) {
+            return res.json({ message: "User already exists" });
+        }
+
+        const user = new User({
+            username,
+            password
+        });
+
+        await user.save();
+
+        res.json({ message: "Registration Successful" });
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.post("/login", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        const user = await User.findOne({
+            username,
+            password
+        });
+
+        if (!user) {
+            return res.json({
+                message: "Invalid Username or Password"
+            });
+        }
+
+        res.json({
+            message: "Login Successful",
+            userId: user._id
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        });
+    }
+});
+
+app.post("/tasks", async (req, res) => {
+    try {
+        const { title, userId } = req.body;
+
+        const task = new Task({
+            title,
+            userId
+        });
+
+        await task.save();
+
+        res.json(task);
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.get("/tasks/:userId", async (req, res) => {
+    try {
+        const tasks = await Task.find({
+            userId: req.params.userId
+        });
+
+        res.json(tasks);
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.put("/tasks/:id", async (req, res) => {
+    try {
+        const updatedTask = await Task.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+
+        res.json(updatedTask);
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+    try {
+        await Task.findByIdAndDelete(req.params.id);
+
+        res.json({
+            message: "Task Deleted"
+        });
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
